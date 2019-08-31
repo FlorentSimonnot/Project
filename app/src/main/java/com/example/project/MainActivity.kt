a7118410d0345bfcd4727126a801966176c9f2e7
FlorentSimonnot <florent.professionnel@outlook.fr>
2019-08-31 02:36:18 +0200

    oups

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -5,10 +5,14 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.*
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import com.example.arrayAdapterCustom.ArrayAdapterCustom
 import com.example.dateCustom.DateCustom
 import com.example.dateCustom.TimeCustom
@@ -18,29 +22,48 @@ import com.example.menu.MenuCustom
 import com.example.notification.MyFirebaseMessagingService
 import com.example.session.SessionUser
 import com.example.user.User
+import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.bottomnavigation.BottomNavigationItemView
 import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener
+import com.google.android.material.internal.NavigationMenuView
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_forgot_password.view.*
+import androidx.drawerlayout.widget.DrawerLayout
 
 
-class MainActivity : AppCompatActivity() {
+
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     /*180719*/
     var session : SessionUser = SessionUser(this)
+    private lateinit var navigationView : NavigationView
+    private lateinit var toolbar : Toolbar
+    private lateinit var drawerLayout : DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_activity)
 
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        /*val navView: BottomNavigationView = findViewById(R.id.nav_view)
         val menu = MenuCustom(this, navView, this@MainActivity)
         navView.setOnNavigationItemSelectedListener(menu.onNavigationItemSelectedListener)
+
+        drawerLayout = findViewById(R.id.drawer)
+        toolbar = findViewById(R.id.toolbar)
+        navigationView = findViewById(R.id.navigation_drawer)
+
+        this.configureDrawerLayout()
+        this.configureNavigationView()
+        this.configureToolBar()
+
+
         /* ----------- BUTTON ACTIONS -------------*/
-        val btnSearch : ImageButton = findViewById(R.id.btn_search)
-        val btnSettings : ImageButton = findViewById(R.id.btn_settings)
+        //val btnSearch : ImageButton = findViewById(R.id.btn_search)
+        //val btnSettings : ImageButton = findViewById(R.id.btn_settings)
 
         if (!session.isLogin()) {
             val logInIntent = Intent(this, LoginActivity::class.java)
@@ -63,7 +86,7 @@ class MainActivity : AppCompatActivity() {
             })
 
 
-            btnSearch.setOnClickListener {
+            /*btnSearch.setOnClickListener {
                 val intent = Intent(this, SearchBarActivity::class.java)
                 startActivity(intent)
             }
@@ -71,7 +94,7 @@ class MainActivity : AppCompatActivity() {
             btnSettings.setOnClickListener {
                 val intent = Intent(this, SettingsActivity::class.java)
                 startActivity(intent)
-            }
+            }*/
 
 
             /* ----------- SHOW EVENTS ---------------*/
@@ -83,7 +106,7 @@ class MainActivity : AppCompatActivity() {
                 val createEventIntent = Intent(this, CreateEventActivity::class.java)
                 startActivity(createEventIntent)
             }
-        }
+        }*/
 
 
     }
@@ -142,4 +165,50 @@ class MainActivity : AppCompatActivity() {
             })
         }
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        when(id){
+            R.id.navigation_account-> {}
+            R.id.navigation_friends -> {}
+            R.id.navigation_events -> {}
+            else ->{}
+        }
+
+        this.drawerLayout.closeDrawer(GravityCompat.START)
+
+        return true
+    }
+
+    private fun configureToolBar() {
+        setSupportActionBar(toolbar)
+    }
+
+    // 2 - Configure Drawer Layout
+    private fun configureDrawerLayout() {
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    // 3 - Configure NavigationView
+    private fun configureNavigationView() {
+        navigationView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
 }
