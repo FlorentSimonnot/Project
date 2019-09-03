373e91455eacfe860f1a46fcb7b85c2a3619ec11
FlorentSimonnot <florent.professionnel@outlook.fr>
2019-09-04 00:33:09 +0200

    map good work

--- app/src/main/java/com/example/project/MapFragment.kt
+++ app/src/main/java/com/example/project/MapFragment.kt
@@ -2,11 +2,17 @@ package com.example.project
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.app.AlertDialog
 import android.content.Context
 import android.content.Context.LOCATION_SERVICE
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.IntentSender
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.location.Criteria
 import android.location.Location
 import android.location.LocationManager
 import android.location.LocationProvider
@@ -19,17 +25,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.getSystemService
+import com.example.arrayAdapterCustom.ArrayAdapterCustom
+import com.example.dateCustom.DateCustom
+import com.example.dateCustom.TimeCustom
+import com.example.events.Event
 import com.example.menu.MenuCustom
+import com.example.place.SessionGooglePlace
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.api.GoogleApiClient
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.FetchPlaceRequest
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 open class MapFragment(
@@ -38,7 +63,15 @@ open class MapFragment(
     lateinit var map : GoogleMap
     lateinit var mapView : MapView
     lateinit var v : View
-    lateinit var locationManager: LocationManager
+    lateinit var fusedLocationClient: FusedLocationProviderClient
+    lateinit var lastLocation : Location
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var locationRequest: LocationRequest
+    private var locationUpdateState = false
+
+    val PERMISSION_LOCATION_REQUEST_CODE = 1
+    val REQUEST_CHECK_SETTINGS = 2
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,16 +83,6 @@ open class MapFragment(
         savedInstanceState: Bundle?
     ): View? {
         v = inflater.inflate(R.layout.fragment_map, container, false)
-        locationManager = activity!!.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-
-        val providers : ArrayList<LocationProvider> =  ArrayList()
-        val names : ArrayList<String> = locationManager.getProviders(true).toList()
-
-        names.forEach {
-            providers.add(locationManager.getProvider(it)!!)
-        }
-
-
         return v
     }
 
@@ -67,32 +90,200 @@ open class MapFragment(
         super.onViewCreated(view, savedInstanceState)
         mapView = view.findViewById(R.id.map) as MapView
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity!!)
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(p0: LocationResult) {
+                super.onLocationResult(p0)
+
+                lastLocation = p0.lastLocation
+                placeMarkerOnMap(LatLng(lastLocation.latitude, lastLocation.longitude), -1)
+            }
+        }
+
+        createLocationRequest()
+
+
         if(mapView != null){
             mapView.onCreate(null)
             mapView.onResume()
             mapView.getMapAsync(this)
         }
-    }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-    }
+        searchEvents()
 
-    override fun onDetach() {
-        super.onDetach()
     }
 
     interface OnFragmentInteractionListener {
         fun onFragmentInteraction(uri: Uri)
     }
 
-    @SuppressLint("MissingPermission")
     override fun onMapReady(p0: GoogleMap?) {
-        MapsInitializer.initialize(context!!)
         map = p0!!
-        map.mapType = GoogleMap.MAP_TYPE_NORMAL
-        val sydney = LatLng(-34.0, 151.0)
-        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        map.uiSettings.isZoomControlsEnabled = true
+
+        setUpMap()
+    }
+
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(context!!,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity!!,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PERMISSION_LOCATION_REQUEST_CODE)
+            return
+        }
+        map.isMyLocationEnabled = true
+        fusedLocationClient.lastLocation.addOnSuccessListener(activity!!) { location ->
+            if (location != null) {
+                lastLocation = location
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+            }
+        }
+    }
+
+
+    private fun startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(context!!,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity!!,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSION_LOCATION_REQUEST_CODE)
+            return
+        }
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null)
+    }
+
+    private fun createLocationRequest() {
+        locationRequest = LocationRequest()
+        locationRequest.interval = 10000
+        locationRequest.fastestInterval = 5000
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+
+        val client = LocationServices.getSettingsClient(activity!!)
+        val task = client.checkLocationSettings(builder.build())
+
+        task.addOnSuccessListener {
+            locationUpdateState = true
+            startLocationUpdates()
+        }
+        task.addOnFailureListener { e ->
+            if (e is ResolvableApiException) {
+                // Location settings are not satisfied, but this can be fixed
+                // by showing the user a dialog.
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+                    e.startResolutionForResult(activity!!, REQUEST_CHECK_SETTINGS)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error.
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CHECK_SETTINGS) {
+            if (resultCode == Activity.RESULT_OK) {
+                locationUpdateState = true
+                startLocationUpdates()
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!locationUpdateState) {
+            startLocationUpdates()
+        }
+    }
+
+    private fun placeMarkerOnMap(location: LatLng, resource : Int?, title : String = "") {
+        if(resource != -1) {
+            val markerOptions = MarkerOptions()
+                .icon(bitmapDescriptorFromVector(context!!, resource!!))
+                .position(location)
+                .title(title)
+            map.addMarker(markerOptions)
+        }
+        else{
+            val markerOptions = MarkerOptions()
+                .position(location)
+            map.addMarker(markerOptions)
+        }
+    }
+
+
+    private fun searchEvents(){
+        val ref = FirebaseDatabase.getInstance().getReference("events")
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val data = dataSnapshot.children //Children = each event
+                val events : ArrayList<Event> = ArrayList()
+                data.forEach {
+                    val event = it.getValue(Event::class.java) //Get event in a Event class
+                    if(event != null){
+                        val date = DateCustom(event.date)
+                        val time = TimeCustom(event.time)
+                        if(date.isEqual(DateCustom("01/01/1971").getCurrentDate())){
+                            if(time.isAfter(TimeCustom("01:01").getCurrentTime())){
+                                events.add(event)
+                            }
+                        }
+                        else if(date.isAfter(DateCustom("01/01/1971").getCurrentDate())){
+                            events.add(event)
+                        }
+                    }
+                }
+                makeEventsOnMap(events)
+
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {}
+        })
+    }
+
+    private fun makeEventsOnMap(events : ArrayList<Event>){
+        events.forEach {
+            val gg = SessionGooglePlace(context!!)
+            gg.init()
+
+            val event = it
+            val placeId = event.place
+            val placesClient = gg.createClient()
+            val placeFields : List<Place.Field> = Arrays.asList(Place.Field.ID, Place.Field.LAT_LNG)
+            val request : FetchPlaceRequest = FetchPlaceRequest.newInstance(placeId, placeFields)
+
+
+            placesClient.fetchPlace(request)
+                .addOnSuccessListener {
+                    val place : Place = it.place
+                    val coords : LatLng? = place.latLng
+                    if(coords != null){
+                        placeMarkerOnMap(coords, event.sport.getLogoSport(), event.name)
+                    }
+                }
+                .addOnFailureListener {
+                }
+        }
+    }
+
+    private fun bitmapDescriptorFromVector(context: Context, vectorResId: Int): BitmapDescriptor? {
+        return ContextCompat.getDrawable(context, vectorResId)?.run {
+            setBounds(0, 0, intrinsicWidth, intrinsicHeight)
+            val bitmap = Bitmap.createBitmap(intrinsicWidth, intrinsicHeight, Bitmap.Config.ARGB_8888)
+            draw(Canvas(bitmap))
+            BitmapDescriptorFactory.fromBitmap(bitmap)
+        }
     }
 
 }
