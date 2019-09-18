f6017d6934d0999bb2598bc4ab453a94e2edcce9
FlorentSimonnot <florent.professionnel@outlook.fr>
2019-09-19 01:36:15 +0200

    bruh

--- app/src/main/java/com/example/project/Tutorial.kt
+++ app/src/main/java/com/example/project/Tutorial.kt
@@ -2,6 +2,8 @@ package com.example.project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
@@ -14,6 +16,7 @@ import java.util.ArrayList
 class Tutorial : AppCompatActivity() {
     private lateinit var viewPager : ViewPager
     private lateinit var pagerAdapter : SliderPageFragment
+    private lateinit var dotsIndicators : LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +24,10 @@ class Tutorial : AppCompatActivity() {
 
         val list : List<Fragment> = listOf(PageFragment1(), PageFragment2(), PageFragment3())
         viewPager = findViewById(R.id.viewPager2)
+        dotsIndicators = findViewById(R.id.pager_dots)
         pagerAdapter = SliderPageFragment(list, supportFragmentManager)
         viewPager.adapter = pagerAdapter
 
     }
+
 }
